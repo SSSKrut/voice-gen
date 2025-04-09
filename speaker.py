65425857cf1e67dcbc27da795f6bf63c63254854
Alexander Nalobin <m_troc@mail.ru>
2025-04-09 22:57:45 +0300

    Comment out TTS-info lines

--- speaker.py
+++ speaker.py
@@ -3,17 +3,19 @@ from TTS.api import TTS
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-print(TTS().list_models())
+## List of available TTS models
+# print(TTS().list_models())
 
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
-print(tts.speakers)
+## List of available TTS speakers
+# print(tts.speakers)
 
 
 # wav = tts.tts(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en")
-
+text = "Привет, мир!"
 tts.tts_to_file(
-    text="Привет, Мир!",
+    text=text,
     speaker_wav="./speaker.wav",
     language="ru",
     file_path="output.mp3",
